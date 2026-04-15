36eaeb798091fe8c5d6ccfd67540266c8cb71ed6
Diana Sofia Regalado Valerio <sofiav1098@gmail.com>
2026-04-15 17:52:40 -0600

    Método init

--- Codigo.py
+++ Codigo.py
@@ -1,1 +1,8 @@
 class Platillo:
+    def __init__(self, id, nombre, descripcion, precio, categoria, ingredientes = []):
+        self.__id = id
+        self.__nombre = nombre
+        self.__descripcion = descripcion
+        self.__precio = precio
+        self.__categoria = categoria
+        self.__ingredientes = ingredientes
